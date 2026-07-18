c25e9f92fbc058007292b67e5a4eb50365598194
luciano444-droid <luciano311210@gmail.com>
2026-07-18 16:10:23 -0300

    Update Sonic Palace of Scorpions.py

--- Sonic Palace of Scorpions.py
+++ Sonic Palace of Scorpions.py
@@ -12,16 +12,21 @@ st.subheader("Bem vindos ao site oficial de Sonic Palace of Scorpions!")
 st.write("Aqui teremos informações sobre as músicas da HQ:")
 st.divider()
 
-st.subheader("Primeira música: Year Zero:")
+st.subheader("Primeira Musica: Year Zero:")
 st.html('<div id="year-zero"></div>')
 st.video("Musicas/lv_0_20260712164740.mp4")
 st.caption("Vídeo da música Year Zero")
 st.write("Creditos: Ghost B.C")
+st.divider()
+st.subheader("Segunda Musica: This Is My Song: ")
+st.video("Musicas/km_20260714-2_1080p_30f_20260714_135737.mp4")
+st.caption("Thi is my song")
 conteudo_site = [
     "Bem vindos ao site oficial de Sonic Palace of Scorpions!",
     "Primeira música: Year Zero:",
-    "Vídeo da música Year Zero"
-    "creditos: Ghost B.C"
+    "Vídeo da música Year Zero",
+    "creditos: Ghost B.C",
+    "Segunda Musica: This Is My Song:"
 ]
 
 if pesquisa:
@@ -31,13 +36,7 @@ if pesquisa:
         if str(pesquisa).lower() in str(texto).lower():
             espaco_resultado.write(f"Resultado encontrado: {texto}")
             encontrado = True
-        if "year zero" in str(texto).lower():
-            # Correção aplicada aqui:
-            with open("/home/lc_4444/Documentos/python/sites/HQ/Audio/Musicas/lv_0_20260712164740.mp4", "rb") as f:
-                video_bytes = f.read()
-            
-            espaco_resultado.video(video_bytes)
-            espaco_resultado.caption("Vídeo da música Year Zero")
+        
             
     if not encontrado:
         espaco_resultado.write("Nenhum resultado encontrado para a sua pesquisa.")
