1ac696a9d325a86652e2dbc54080b5e32f2bec9b
luciano444-droid <luciano311210@gmail.com>
2026-07-18 15:18:17 -0300

    Update Sonic Palace of Scorpions.py

--- Sonic Palace of Scorpions.py
+++ Sonic Palace of Scorpions.py
@@ -16,10 +16,12 @@ st.subheader("Primeira música: Year Zero:")
 st.html('<div id="year-zero"></div>')
 st.video("Musicas/lv_0_20260712164740.mp4")
 st.caption("Vídeo da música Year Zero")
+st.write("Creditos: Ghost B.C")
 conteudo_site = [
     "Bem vindos ao site oficial de Sonic Palace of Scorpions!",
     "Primeira música: Year Zero:",
     "Vídeo da música Year Zero"
+    "creditos: Ghost B.C"
 ]
 
 if pesquisa:
